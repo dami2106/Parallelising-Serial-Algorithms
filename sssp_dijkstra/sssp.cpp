684ceaa47df9014bd06429500897927af7d8587f
2303289 <2303289@students.wits.ac.za>
2022-09-29 23:36:11 +0200

    Finished serial dijsktra

--- sssp_dijkstra/sssp.cpp
+++ sssp_dijkstra/sssp.cpp
@@ -2,21 +2,75 @@
 #include <vector>
 #include <sstream>
 #include <array>
-#include <set>
+#include <unordered_set>
+
 
 using namespace std;
 
 vector<vector<int> > makeGraph(int &vertexCount, int &edgeCount);
 
+void printPath(int vert, vector<int> parents);
+
+void printSolution(int startVertex, vector<int> distances, vector<int> parents);
+
 int main() {
-    int vertexCount, edgeCount;
-    vector<vector<int> > adj = makeGraph(vertexCount, edgeCount);;
-    set<int> visited;
-    for(int i = 1 ; i < vertexCount ; i++) {
-        
+    int vertexCount, edgeCount, startVertex = 0;
+    vector<vector<int> > adj = makeGraph(vertexCount, edgeCount);
+    unordered_set<int> vT;
+    vector<int> dist(vertexCount, INT_MAX);
+    vector<int> parents(vertexCount);
+    dist[startVertex] = 0;
+    parents[startVertex] = -1;
+
+    while (vT.size() != vertexCount) {
+        int u, min = INT_MAX;
+
+        for (int i = 0; i < vertexCount; i++)
+            if (vT.find(i) == vT.end() && dist[i] < min) min = dist[i], u = i;
+
+        vT.insert(u);
+
+        for (int v = 0; v < vertexCount; v++) {
+            if (adj[v][u] != -1)
+                if (vT.find(v) == vT.end() && dist[v] > dist[u] + adj[v][u]) {
+                    dist[v] = dist[u] + adj[v][u];
+                    parents[v] = u;
+                }
+        }
     }
+    printSolution(startVertex, dist, parents);
+}
 
+/*
+ * Prints the path from start to vertex
+ */
+void printPath(int vert, vector<int> parents) {
+    if (vert == -1)
+        return;
+    printPath(parents[vert], parents);
+    cout << vert << " ";
+}
 
+/*
+ * Prints the entirety of the solution
+ */
+void printSolution(int startVertex, vector<int> distances, vector<int> parents) {
+    int nVertices = distances.size();
+
+    cout << "The distance from " << startVertex << " to each vertex is:\n";
+    cout << "  v    dist " << startVertex << "->v\n----   ---------\n";
+    for (int vertexIndex = 0; vertexIndex < nVertices; vertexIndex++)
+        if (vertexIndex != startVertex) cout << vertexIndex << ":\t " << distances[vertexIndex] << "\n";
+
+    cout << "\n\nThe shortest path from " << startVertex << " to each vertex is:\n";
+    cout << "  v    dist " << startVertex << "->v\n----   ---------\n";
+    for (int vertexIndex = 0; vertexIndex < nVertices; vertexIndex++) {
+        if (vertexIndex != startVertex) {
+            cout << vertexIndex << ":\t ";
+            printPath(vertexIndex, parents);
+            cout << "\n";
+        }
+    }
 }
 
 /*
@@ -29,13 +83,13 @@ vector<vector<int> > makeGraph(int &vertexCount, int &edgeCount) {
     getline(cin, initLine);
     stringstream ss(initLine);
 
-    while(ss >> word)
+    while (ss >> word)
         graphInfo.push_back(stoi(word));
 
     vertexCount = graphInfo[0];
     edgeCount = graphInfo[1];
 
-    vector< vector<int> > adj(vertexCount, vector<int>(vertexCount, -1));
+    vector<vector<int> > adj(vertexCount, vector<int>(vertexCount, -1));
     for (int i = 0; i < edgeCount; ++i) {
         string currNode, nodeItem;
         array<int, 3> nodeInfo;
@@ -44,11 +98,12 @@ vector<vector<int> > makeGraph(int &vertexCount, int &edgeCount) {
         getline(cin, currNode);
         stringstream sx(currNode);
 
-        while (sx >> nodeItem){
+        while (sx >> nodeItem) {
             nodeInfo[k] = stoi(nodeItem);
             ++k;
         }
         adj[nodeInfo[0]][nodeInfo[1]] = nodeInfo[2];
+        adj[nodeInfo[1]][nodeInfo[0]] = nodeInfo[2];
     }
 
     return adj;
